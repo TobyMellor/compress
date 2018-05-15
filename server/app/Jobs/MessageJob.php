a3133c5214d2a1e123070d67445cc1dd793a372f
TobyMellor <tobymulberry@hotmail.com>
2018-05-15 19:38:00 +0100

    Remove all references to tokens

--- server/app/Jobs/MessageJob.php
+++ server/app/Jobs/MessageJob.php
@@ -45,17 +45,5 @@ class MessageJob extends Job
         $topic->topic('notifications');
 
         $downstreamResponse = FCM::sendToTopic($topic, null, $notification, $data);
-
-        //return Array - you must remove all this tokens in your database
-        var_dump($downstreamResponse->tokensToDelete());
-
-        //return Array (key : oldToken, value : new token - you must change the token in your database )
-        var_dump($downstreamResponse->tokensToModify());
-
-        //return Array - you should try to resend the message to the tokens in the array
-        var_dump($downstreamResponse->tokensToRetry());
-
-        // return Array (key:token, value:errror) - in production you should remove from your database the tokens present in this array
-        var_dump($downstreamResponse->tokensWithError());
     }
 }
